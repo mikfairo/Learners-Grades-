116fd46247eb2a65432f3fc2a423d13ce3072bea
mikfairo <mikfairo123@gmail.com>
2025-09-08 00:07:31 -0400

    Worked on organizing data into their own arrays based off learner IDs in an if statement

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const CourseInfo = {
   id: 451,
-  name: "Introduction to JavaScript"
+  name: "Introduction to JavaScript",
 };
 
 // The provided assignment group.
@@ -14,21 +14,21 @@ const AssignmentGroup = {
       id: 1,
       name: "Declare a Variable",
       due_at: "2023-01-25",
-      points_possible: 50
+      points_possible: 50,
     },
     {
       id: 2,
       name: "Write a Function",
       due_at: "2023-02-27",
-      points_possible: 150
+      points_possible: 150,
     },
     {
       id: 3,
       name: "Code the World",
       due_at: "3156-11-15",
-      points_possible: 500
-    }
-  ]
+      points_possible: 500,
+    },
+  ],
 };
 
 // The provided learner submission data.
@@ -38,63 +38,89 @@ const LearnerSubmissions = [
     assignment_id: 1,
     submission: {
       submitted_at: "2023-01-25",
-      score: 47
-    }
+      score: 47,
+    },
   },
   {
     learner_id: 125,
     assignment_id: 2,
     submission: {
       submitted_at: "2023-02-12",
-      score: 150
-    }
+      score: 150,
+    },
   },
   {
     learner_id: 125,
     assignment_id: 3,
     submission: {
       submitted_at: "2023-01-25",
-      score: 400
-    }
+      score: 400,
+    },
   },
   {
     learner_id: 132,
     assignment_id: 1,
     submission: {
       submitted_at: "2023-01-24",
-      score: 39
-    }
+      score: 39,
+    },
   },
   {
     learner_id: 132,
     assignment_id: 2,
     submission: {
       submitted_at: "2023-03-07",
-      score: 140
-    }
-  }
+      score: 140,
+    },
+  },
 ];
 
-function getLearnerData(course, ag, submissions) { // sample of what the final input should look like in console
+// console.log(AssignmentGroup);
+// console.log(AssignmentGroup.assignments[0].id);
+
+// let assignment1 =
+// let assignment2 =
+
+function getLearnerData(course, avg, submissions) {
+  // sample of what the final input should look like in console
   // here, we would process this data to achieve the desired result.
-  const result = [
-    {
-      id: 125,
-      avg: 0.985, // (47 + 150) / (50 + 150)
-      1: 0.94, // 47 / 50
-      2: 1.0 // 150 / 150
-    },
-    {
-      id: 132,
-      avg: 0.82, // (39 + 125) / (50 + 150)
-      1: 0.78, // 39 / 50
-      2: 0.833 // late: (140 - 15) / 150
-    }
-  ];
-
-  return result;
+  //   const result = [
+  //     {
+  //       id: 125,
+  //       avg: 0.985, // (47 + 150) / (50 + 150)
+  //       1: 0.94, // 47 / 50
+  //       2: 1.0 // 150 / 150
+  //     },
+  //     {
+  //       id: 132,
+  //       avg: 0.82, // (39 + 125) / (50 + 150)
+  //       1: 0.78, // 39 / 50
+  //       2: 0.833 // late: (140 - 15) / 150
+  //     }
+  //   ];
+  //   return result;
 }
+let learnerArray = [];
 
-const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+for (const item of LearnerSubmissions) {
 
-console.log(result);
+  let idExists = false;
+
+  for (const obj of learnerArray) {
+    if (item.learner_id === obj.id) {
+      obj[item.assignment_id] = item.submission.score
+      idExists = true;
+      break
+    }
+  }
+  if (idExists === false) {
+  let studentObject = {
+    
+    id: item.learner_id,
+    avg: 0,
+    [item.assignment_id]: item.submission.score,
+  };
+  learnerArray.push(studentObject); //adding to the learnerData array
+}
+}
+console.log(learnerArray);
